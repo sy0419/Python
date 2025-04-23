4d97e1a8b63504eb659e9c4a58f9dd5ba382a3ec
sy0419 <psy041912@gmail.com>
2025-04-23 23:23:49 +0900

    Distinguish odd and even numbers.

---
+++ python-basics/level0/9_oddEven.py
@@ -0,0 +1,8 @@
+a = int(input())
+if a % 2 == 1:
+    print(f"{a} is odd.")
+else:
+    print(f"{a} is even.")
+
+# when 'a' divides 2, the remainder is 1 which 'a' is odd. The other case is even.
+# a를 2로 나누었을 때 나머지가 1이면 홀수, 나머지가 0이면 짝수.
