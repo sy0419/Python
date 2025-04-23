3c23f131f47bcdeecdad52b4e2c80731db8f8eb0
sy0419 <psy041912@gmail.com>
2025-04-23 23:07:19 +0900

    Input a and b, and then print 'a', 'b' values with 'a + b' value.

--- python-basics/level0/6_printA+B.py
+++ python-basics/level0/6_printA+B.py
@@ -1,2 +1,5 @@
 a, b = map(int, input().strip().split(' '))
 print(f"{a} + {b} = {a + b}")
+
+# f-string allows you to insert variables directly into a string using {}.
+# f-string는 파이썬의 문자열 포매팅 방법 중 하나로, 중괄호 {} 안에 변수를 직접 넣을 수 있다.
