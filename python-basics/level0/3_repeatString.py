5b5a137bdb088eab799aa66345d3ad2368a5b2b1
sy0419 <psy041912@gmail.com>
2025-04-23 21:41:26 +0900

    Input String str and integer n, and then print str repeat n times.

--- python-basics/level0/3_repeatString.py
+++ python-basics/level0/3_repeatString.py
@@ -1,3 +1,6 @@
 str, n = input().strip().split(' ')
 n = int(n)
 print(str * n)
+
+# Get input from the user and split it using spaces then put into a list.
+# 사용자로부터 입력을 받아서 공백 기준으로 나눈 리스트로 만드는 과정이야.
