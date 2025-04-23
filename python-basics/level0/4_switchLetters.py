aa2b6664397226a8e24b22ba2f35463a0aa70f80
sy0419 <psy041912@gmail.com>
2025-04-23 22:46:14 +0900

    Switch Upper letters to Lower letters and Lower letters to Upper letters.

--- python-basics/level0/4_switchLetters.py
+++ python-basics/level0/4_switchLetters.py
@@ -1,2 +1,6 @@
 str = input()
 print(str.swapcase())
+
+# swapcase() method converts all uppercase letters to lowercase, and all lowercase letters to uppercase.
+# swapcase() 메서드는 문자열에서 대문자는 소문자로, 소문자는 대문자로 바꿔주는 함수.
+# 숫자나 특수문자는 그대로 남아있다.
