0bec30c664d6957dcd56b0b18fed82fafea1beea
sy0419 <psy041912@gmail.com>
2025-04-23 23:12:59 +0900

    Input string and then show the string in a row.

--- python-basics/level0/8_stringLineUp.py
+++ python-basics/level0/8_stringLineUp.py
@@ -1,3 +1,4 @@
 str = input()
-for letter in str:
+for letter in str: # for loop that iterates through each character(letter) in the string(str) one by one.
+                   # 문자열 str의 각 문자(letter)를 하나씩 꺼내서 반복문에 실행
     print(letter)
