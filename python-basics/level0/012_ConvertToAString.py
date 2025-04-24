f40a072643833ab41e756f9f2fbf55e3418a9477
sy0419 <psy041912@gmail.com>
2025-04-25 00:30:59 +0900

    Convert a list of characters to a string.

---
+++ python-basics/level0/012_ConvertToAString.py
@@ -0,0 +1,11 @@
+# 문자들이 담겨있는 배열 arr가 주어집니다. 
+# arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+def solution(arr):
+    answer = ''
+    for string in arr:
+        answer += string
+    return answer
+
+arr = ["a", "b", "c"]
+result = solution(arr)
+print(result)
