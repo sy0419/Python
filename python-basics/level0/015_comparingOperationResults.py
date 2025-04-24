dbce973046cf3c314c9375da553bf79ccc1a39f0
sy0419 <psy041912@gmail.com>
2025-04-25 00:56:28 +0900

    Compare the operation values ​​of two numbers.

---
+++ python-basics/level0/015_comparingOperationResults.py
@@ -0,0 +1,20 @@
+# 연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+# 12 ⊕ 3 = 123 , 3 ⊕ 12 = 312
+# 양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
+# 단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다.
+# 제한사항) 1 ≤ a, b < 10,000
+
+def solution(a, b):
+    strA = str(a)
+    strB = str(b)
+    ab = strA + strB
+    ab2 = 2 * a * b
+    if int(ab) > ab2:
+        return ab
+    else:
+        return ab2
+
+a = int(input("a: "))
+b = int(input("b: "))
+result = solution(a, b)
+print(result)
