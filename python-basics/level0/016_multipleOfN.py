7cbe6ff014474180f35f2954ad5489e737f65964
sy0419 <psy041912@gmail.com>
2025-04-25 15:42:35 +0900

    Find that num is multiple of n.

---
+++ python-basics/level0/016_multipleOfN.py
@@ -0,0 +1,13 @@
+# 정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return 
+# n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+# 제한사항) 2 ≤ num ≤ 100 , 2 ≤ n ≤ 9
+def solution(num, n):
+    if num % n == 0:
+        return 1
+    else:
+        return 0
+
+num = int(input("num: "))
+n = int(input("n: "))
+result = solution(num, n)
+print(result)
