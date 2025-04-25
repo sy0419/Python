31728b738f8f794f01bf4b805c0bfe04dadbe9e4
sy0419 <psy041912@gmail.com>
2025-04-25 16:40:05 +0900

    Depend on flag, print different values.

---
+++ python-basics/level0/020_dependOnFlag.py
@@ -0,0 +1,20 @@
+# 두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, 
+# flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+# 제한사항) -1,000 ≤ a, b ≤ 1,000
+
+def solution(a, b, flag):
+    if flag == True:
+        return a + b
+    else:
+        return a - b
+
+a = int(input("a: "))
+b = int(input("b: "))
+flag_input = input("flag (true/false): ").lower()
+flag = flag_input == "true"
+result = solution(a, b, flag)
+print(result)
+
+# a	   b	flag	result
+# -4   7	true	  3
+# -4   7	false	 -11
