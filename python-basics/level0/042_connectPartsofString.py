8159c13c91cae1398665362b668f07dac5c583f8
sy0419 <psy041912@gmail.com>
2025-04-27 23:20:13 +0900

    Print a combination strings from two diffierent lists.

---
+++ python-basics/level0/042_connectPartsofString.py
@@ -0,0 +1,17 @@
+# 길이가 같은 문자열 배열 my_strings와 이차원 정수 배열 parts가 매개변수로 주어집니다. 
+# parts[i]는 [s, e] 형태로, my_string[i]의 인덱스 s부터 인덱스 e까지의 부분 문자열을 의미합니다.
+# 각 my_strings의 원소의 parts에 해당하는 부분 문자열을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+# 제한사항
+# 1 ≤ my_strings의 길이 = parts의 길이 ≤ 100
+# 1 ≤ my_strings의 원소의 길이 ≤ 100
+# parts[i]를 [s, e]라 할 때, 다음을 만족합니다.
+# 0 ≤ s ≤ e < my_strings[i]의 길이
+
+def solution(my_string, parts):
+    answer = ''
+    for string, part in zip(my_string, parts): #zip makes two different list to run at the same time , zip은 두 리스트를 동시에 묶어서 반복할 수 있도록 한다.
+        s, e = part
+        answer += string[s:e+1]
+    return answer
+
+print(solution(["progressive", "hamburger", "hammer", "ahocorasick"], [[0, 4], [1, 2], [3, 5], [7, 7]]))
