d4d2cc380108c68046f8626f5a4837d4fa7a994f
sy0419 <psy041912@gmail.com>
2025-05-11 21:22:43 +0900

    Convert a sequence which conditions are accepted.

---
+++ python-basics/level0/070_convertingASequence1.py
@@ -0,0 +1,16 @@
+# 정수 배열 arr가 주어집니다. arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고, 
+# 50보다 작은 홀수라면 2를 곱합니다. 그 결과인 정수 배열을 return 하는 solution 함수를 완성해 주세요.
+# 제한사항) 1 ≤ arr의 길이 ≤ 1,000,000 , 1 ≤ arr의 원소의 값 ≤ 100
+
+def solution(arr):
+    answer = []
+    for num in arr:
+        if num >= 50 and num % 2 == 0:
+                answer.append(num // 2)
+        elif num < 50 and num % 2 == 1:
+             answer.append(num * 2)
+        else:
+             answer.append(num)
+    return answer
+
+print(solution([1, 2, 3, 100, 99, 98]))
