834f71b1e7c9cc4ec8ec8a80ecbc59ebc7140cee
sy0419 <psy041912@gmail.com>
2025-05-11 20:34:27 +0900

    Print a list containing the elements stored in order from the first element of 'num_list' to the last element at 'n' intervals.

---
+++ python-basics/level0/064_intervalsN.py
@@ -0,0 +1,12 @@
+# 정수 리스트 num_list와 정수 n이 주어질 때, num_list의 첫 번째 원소부터 마지막 원소까지 n개 간격으로 
+# 저장되어있는 원소들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+# 제한사항
+# 5 ≤ num_list의 길이 ≤ 20
+# 1 ≤ num_list의 원소 ≤ 9
+# 1 ≤ n ≤ 4
+
+def solution(num_list, n):
+    return num_list[::n]
+
+print(solution([4, 2, 6, 1, 7, 6], 2))
+print(solution([4, 2, 6, 1, 7, 6], 4))
