c27090927213b4d9225b9d757a19061b658a32f2
sy0419 <psy041912@gmail.com>
2025-05-11 20:30:16 +0900

    Print the list 'num_list' which is just before index 'n'.

---
+++ python-basics/level0/063_printJustBeforeN.py
@@ -0,0 +1,12 @@
+# 정수 리스트 num_list와 정수 n이 주어질 때, 
+# num_list의 첫 번째 원소부터 n 번째 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+# 제한사항
+# 2 ≤ num_list의 길이 ≤ 30
+# 1 ≤ num_list의 원소 ≤ 9
+# 1 ≤ n ≤ num_list의 길이 ___
+
+def solution(num_list, n):
+    return num_list[:n]
+
+print(solution([2, 1, 6], 1))
+print(solution([5, 2, 1, 7, 5], 3))
