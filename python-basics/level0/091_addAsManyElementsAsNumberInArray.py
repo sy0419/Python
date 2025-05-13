ba45e0d194fa3e7f7943f74d31d7bd16c6fc3ac0
sy0419 <psy041912@gmail.com>
2025-05-14 01:56:47 +0900

    Print repeat strings as much as a number in array 'arr'.

---
+++ python-basics/level0/091_addAsManyElementsAsNumberInArray.py
@@ -0,0 +1,26 @@
+# 아무 원소도 들어있지 않은 빈 배열 X가 있습니다. 양의 정수 배열 arr가 매개변수로 주어질 때, 
+# arr의 앞에서부터 차례대로 원소를 보면서 원소가 a라면 X의 맨 뒤에 a를 a번 추가하는 일을 반복한 뒤의 배열 X를 return 하는 solution 함수를 작성해 주세요.
+# 제한사항) 1 ≤ arr의 길이 ≤ 100 , 1 ≤ arr의 원소 ≤ 100
+
+def solution(arr):
+    x = []
+    for num in arr:
+        for _ in range(num):
+            x.append(num)
+    return x
+
+def solution2(arr):
+    x = []
+    for num in arr:
+        x.extend([num] * num)  # num을 num개만큼 가진 리스트를 한 번에 붙이기
+    return x
+
+print(solution([5, 1, 4]))
+print(solution([6, 6]))
+print(solution([1]))
+print("-------------------------------------")
+print(solution([5, 1, 4]))
+print(solution([6, 6]))
+print(solution([1]))
+
+#  extend()는 다른 반복 가능한 객체(리스트, 튜플 등)의 요소들을 현재 리스트에 추가하는 함수
