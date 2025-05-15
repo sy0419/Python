913fe06e023f37c34cdf1946e761369f7aaea9d1
sy0419 <psy041912@gmail.com>
2025-05-15 16:26:20 +0900

    Print 5 results from back as descending order.

---
+++ python-basics/level0/099_Pick5fromBack.py
@@ -0,0 +1,7 @@
+# 정수로 이루어진 리스트 num_list가 주어집니다. num_list에서 가장 작은 5개의 수를 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+# 제한사항) 6 ≤ num_list의 길이 ≤ 30 , 1 ≤ num_list의 원소 ≤ 100
+
+def solution(num_list):
+    return sorted(num_list)[:5]
+
+print(solution([12, 4, 15, 46, 38, 1, 14]))
